7623b04e2eeba43952310819026f6f9addc4250e
ABBOSJ0N <abbosjonhidoyatov@gmail.com>
2021-12-09 17:48:06 +0500

    yangi casw

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+# # Create your views here.
+# from django.http import HttpResponse
 
+# def home(request):
+#     return HttpResponse('<h1>Blog home</h1>')
+
+# def about(request):
+#     return HttpResponse('<h1>Blog About</h1>')
+posts =[
+    {
+        'author':'Abbos',
+        'title':'Blog post 1',
+        'content':'Birinchi postning matni',
+        'date_posted':'Dekabr 9, 2021'
+
+    },
+]
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
-
+    context={'posts':post}
+    return render(request, 'blog/home.html', context)
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
